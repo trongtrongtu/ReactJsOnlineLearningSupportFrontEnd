20528982dbb796a3d45db40897c30527862ca633
Tuan.LA <luonganhtuan1051999@gmail.com>
2020-06-06 18:29:55 +0700

    update create room, add proxy in package.json

--- src/Component/ListTeams.js
+++ src/Component/ListTeams.js
@@ -9,7 +9,6 @@ export default class ListTeams extends Component {
 
         return (
 
-
             <React.Fragment>
                
                 <div className="py-5">
--- src/Component/createGroup.js
+++ src/Component/createGroup.js
@@ -1,32 +1,64 @@
 import React, { Component } from 'react';
 import './createGroup.css';
 import './createGroupJS.js';
+import axios from 'axios' ;
+
+const addRoomAction =(username,roomNameCreate,passwordRoom)=>(axios.post('/create_room',{username,roomNameCreate,passwordRoom}).then((resp)=>resp.data));
 export default class createGroup extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username:'tuanla99',
+      roomNameCreate: '',
+      passwordRoom:'',
+      passwordRoom_1:''
+    }
+  }
+  isChange =(event) =>{
+    var name = event.target.name ;
+    var value=event.target.value ;
+    console.log(name);
+    console.log(value);
+    this.setState({
+      [name]:value
+    });
+
+  }
+  handleOnClick =() =>{
+    console.log(JSON.stringify(this.state));
+    if(this.state.passwordRoom === this.state.passwordRoom_1){
+      var {username,roomNameCreate,passwordRoom,passwordRoom_1} = this.state;
+        addRoomAction(username,roomNameCreate,passwordRoom).then((respive) =>{console.log(respive);})
+    }else{
+      alert("Nhap Password khong khop.");
+    }
+  }
      render() {
+
           return (
                <form>
         <label>
           <p className="label-txt">ENTER YOUR ROOM</p>
-          <input type="text" className="input" />
+          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="roomNameCreate" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
         <label>
           <p className="label-txt">ENTER YOUR PASSWORD</p>
-          <input type="text" className="input" />
+          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="passwordRoom" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
         <label>
           <p className="label-txt">ENTER YOUR PASSWORD</p>
-          <input type="text" className="input" />
+          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="passwordRoom_1" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
-        <button type="submit">submit</button>
+        <button type="reset" onClick={()=>this.handleOnClick()}>submit</button>
       </form>
           )
      }
