a7f53c7d8260639332e78170361f9480a2e7b01a
trongtrongtu <trongtrongtu169@gmail.com>
2020-06-09 10:13:49 +0700

    Modified Create And Join Room

--- src/Component/JoinGroup.js
+++ src/Component/JoinGroup.js
@@ -1,68 +1,71 @@
 import React, { Component } from 'react';
 import './createGroup.css';
 import './createGroupJS.js';
-import axios from 'axios' ;
+import axios from 'axios';
 
-const addRoomAction =(username,roomNameJoin,passwordRoom)=>(axios.post('/join_room',{username,roomNameJoin,passwordRoom}).then((resp)=>resp.data));
+const addRoomAction = (username, roomNameJoin, passwordRoom) => (axios.post('/join_room', { username, roomNameJoin, passwordRoom }).then((resp) => resp.data));
 export default class JoinGroup extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      username:'tuanla99',
+    this.state = {
+      username: sessionStorage.getItem('user_login'),
       roomNameJoin: '',
-      passwordRoom:''
-     
+      passwordRoom: '',
+      errors: ''
     }
   }
-  isChange =(event) =>{
-    var name = event.target.name ;
-    var value=event.target.value ;
+  isChange = (event) => {
+    var name = event.target.name;
+    var value = event.target.value;
     console.log(name);
     console.log(value);
     this.setState({
-      [name]:value
+      [name]: value
     });
 
   }
-  handleOnClick =() =>{
+  handleOnClick = () => {
     console.log(JSON.stringify(this.state));
-   
-      
-        addRoomAction(this.state.username,this.state.roomNameJoin,this.state.passwordRoom).then((respive) =>{console.log(respive);
-        if(respive.result === 'ok'){
-          this.props.history.push('/Teams') ;
-        }else if(respive.result === 'failed_joined'){
-           alert("User joined the room.");
-        }else if(respive.result === 'failed_roomName'){
-            alert("RoomName wrong.");
-        }else if(respive.result === 'failed_password'){
-            alert("Password wrong") ;
-        }
-        
-        })
-    
-  }
-     render() {
 
-          return (
-               <form>
+
+    addRoomAction(this.state.username, this.state.roomNameJoin, this.state.passwordRoom).then((respive) => {
+      console.log(respive);
+      if (respive.result === 'ok') {
+        this.props.history.push('/Teams');
+      } else if (!this.state.roomNameJoin || !this.state.passwordRoom){
+        this.setState({ errors: 'Cần nhập đủ các trường' })
+      } else if (respive.result === 'failed_joined') {
+        this.setState({ errors: 'Bạn đã tham gia nhóm từ trước' })
+      } else if (respive.result === 'failed_roomName') {
+        this.setState({ errors: 'Tên nhóm không tồn tại' })
+      } else if (respive.result === 'failed_password') {
+        this.setState({ errors: 'Mật khẩu sai' })
+      }
+
+    })
+
+  }
+  render() {
+
+    return (
+      <form>
         <label>
-          <p className="label-txt">ENTER YOUR ROOM</p>
-          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="roomNameJoin" />
+          <p className="label-txt" style={{fontSize:'20px', fontWeight:'700'}}>Tên Nhóm: </p>
+          <input onChange={(event) => this.isChange(event)} type="text" className="input" name="roomNameJoin" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
         <label>
-          <p className="label-txt">ENTER YOUR PASSWORD</p>
-          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="passwordRoom" />
+          <p className="label-txt" style={{fontSize:'20px', fontWeight:'700',marginTop:'15px'}}>Mật Khẩu: </p>
+          <input onChange={(event) => this.isChange(event)} type="password" className="input" name="passwordRoom" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
-       
-        <button type="reset" onClick={()=>this.handleOnClick()}>submit</button>
+        <div style={{ color: "red", marginBottom:'20px' }}>{this.state.errors}</div>
+        <button type="reset" onClick={() => this.handleOnClick()}>THAM GIA</button>
       </form>
-          )
-     }
+    )
+  }
 }
--- src/Component/createGroup.js
+++ src/Component/createGroup.js
@@ -1,69 +1,77 @@
 import React, { Component } from 'react';
 import './createGroup.css';
 import './createGroupJS.js';
-import axios from 'axios' ;
+import axios from 'axios';
 
-const addRoomAction =(username,roomNameCreate,passwordRoom)=>(axios.post('/create_room',{username,roomNameCreate,passwordRoom}).then((resp)=>resp.data));
+const addRoomAction = (username, roomNameCreate, passwordRoom) => (axios.post('/create_room', { username, roomNameCreate, passwordRoom }).then((resp) => resp.data));
 export default class createGroup extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      username:'tuanla99',
+    this.state = {
+      username: sessionStorage.getItem('user_login'),
       roomNameCreate: '',
-      passwordRoom:'',
-      passwordRoom_1:''
+      passwordRoom: '',
+      passwordRoom_1: '',
+      errors: ''
     }
   }
-  isChange =(event) =>{
-    var name = event.target.name ;
-    var value=event.target.value ;
+  isChange = (event) => {
+    var name = event.target.name;
+    var value = event.target.value;
     console.log(name);
     console.log(value);
     this.setState({
-      [name]:value
+      [name]: value
     });
 
   }
-  handleOnClick =() =>{
+  handleOnClick = () => {
     console.log(JSON.stringify(this.state));
-    if(this.state.passwordRoom === this.state.passwordRoom_1){
-      
-        addRoomAction(this.state.username,this.state.roomNameCreate,this.state.passwordRoom).then((respive) =>{console.log(respive);
-        if(respive.result === 'ok'){
-          this.props.history.push('/Teams') ;
-        }
+    if (!this.state.roomNameCreate || !this.state.passwordRoom || !this.state.passwordRoom_1) {
+      this.setState({ errors: 'Cần nhập đủ các trường' })
+    } else {
+      if (this.state.passwordRoom === this.state.passwordRoom_1) {
+        addRoomAction(this.state.username, this.state.roomNameCreate, this.state.passwordRoom).then((respive) => {
+          console.log(respive);
+          if (respive.result === 'ok') {
+            this.props.history.push('/Teams');
+          } else if (respive.result == 'failed') {
+            this.setState({ errors: 'Tên nhóm đã tồn tại' })
+          }
         })
-    }else{
-      alert("Nhap Password khong khop.");
+      } else {
+        this.setState({ errors: "Mật khẩu không khớp" });
+      }
     }
   }
-     render() {
+  render() {
 
-          return (
-               <form>
+    return (
+      <form>
         <label>
-          <p className="label-txt">ENTER YOUR ROOM</p>
-          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="roomNameCreate" />
+          <p className="label-txt" style={{ fontSize: '20px', fontWeight: '700' }}>Tên Nhóm: </p>
+          <input onChange={(event) => this.isChange(event)} type="text" className="input" name="roomNameCreate" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
         <label>
-          <p className="label-txt">ENTER YOUR PASSWORD</p>
-          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="passwordRoom" />
+          <p className="label-txt" style={{ fontSize: '20px', fontWeight: '700', marginTop: '15px' }}>Mật Khẩu: </p>
+          <input onChange={(event) => this.isChange(event)} type="text" className="input" name="passwordRoom" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
         <label>
-          <p className="label-txt">ENTER YOUR PASSWORD</p>
-          <input onChange={(event)=> this.isChange(event)} type="text" className="input" name="passwordRoom_1" />
+          <p className="label-txt" style={{ fontSize: '20px', fontWeight: '700', marginTop: '15px' }}>Nhâp Lại Mật Khẩu: </p>
+          <input onChange={(event) => this.isChange(event)} type="text" className="input" name="passwordRoom_1" />
           <div className="line-box">
             <div className="line" />
           </div>
         </label>
-        <button type="reset" onClick={()=>this.handleOnClick()}>submit</button>
+        <div style={{ color: "red", marginBottom: '20px' }}>{this.state.errors}</div>
+        <button type="reset" onClick={() => this.handleOnClick()}>TẠO</button>
       </form>
-          )
-     }
+    )
+  }
 }
